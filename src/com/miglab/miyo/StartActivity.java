dd9d79d094520f9386e93999c2e7e55b688fc9ba
kerry <kerry@miglab.com>
2015-05-07 17:38:23 +0800

    android基本

---
+++ src/com/miglab/miyo/StartActivity.java
@@ -0,0 +1,16 @@
+package com.miglab.miyo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_start);
+    }
+
+}
